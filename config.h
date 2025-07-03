83e58b39d01f66604a3be7763f0894ec8457494a
Camilo Garcia <camilo_7@msn.com>
2025-07-03 10:22:02 +0200

    qutebrowser scratchpad

--- config.h
+++ config.h
@@ -28,9 +28,8 @@ static const Rule rules[] = {
      */
     /* class      instance    title       tags mask     isfloating   monitor
        scratch key */
-    {"Gimp", NULL, NULL, 0, 1, -1, 0},
-    {"firefox", NULL, NULL, 1 << 8, 0, -1, 0},
-    {NULL, NULL, "scratchpad", 0, 1, -1, 'a'},
+    // {NULL, NULL, "scratchpad", 0, 1, -1, 'a'},
+		{"qutebrowser", NULL, NULL, 0, 1, -1, 'q'}
 };
 
 /* layout(s) */
@@ -71,23 +70,24 @@ static const char *dmenucmd[] = {
 static const char *termcmd[] = {"kitty", NULL};
 
 /*First arg only serves to match against key in rules*/
-static const char *scratchpadcmd[] = {"a", "kitty", "-t", "scratchpad", NULL};
+static const char *scratchpadcmd[] = {
+	// "a", "kitty", "-t", "scratchpad", NULL
+	"q", "qutebrowser", NULL
+};
 
 static const Key keys[] = {
     /* modifier                     key        function        argument */
     {MODKEY, XK_p, spawn, {.v = dmenucmd}},
     {MODKEY, XK_Return, spawn, {.v = termcmd}},
-    {MODKEY, XK_w, togglescratch, {.v = scratchpadcmd}},
-    {MODKEY, XK_b, togglebar, {0}},
-    {MODKEY, XK_j, focusstack, {.i = +1}},
+    {MODKEY, XK_w, togglescratch, {.v = scratchpadcmd[0]}},
+    {MODKEY, XK_b, togglebar, {0}}, {MODKEY, XK_j, focusstack, {.i = +1}},
     {MODKEY, XK_k, focusstack, {.i = -1}},
     {MODKEY | ShiftMask, XK_period, incnmaster, {.i = +1}},
     {MODKEY | ShiftMask, XK_comma, incnmaster, {.i = -1}},
     {MODKEY, XK_h, setmfact, {.f = -0.05}},
     {MODKEY, XK_l, setmfact, {.f = +0.05}},
     // { MODKEY,                       XK_Return, zoom,           {0} },
-    {MODKEY, XK_Tab, view, {0}},
-    {MODKEY, XK_q, killclient, {0}},
+    {MODKEY, XK_Tab, view, {0}}, {MODKEY, XK_q, killclient, {0}},
     {MODKEY, XK_t, setlayout, {.v = &layouts[0]}},
     {MODKEY, XK_f, setlayout, {.v = &layouts[1]}},
     {MODKEY, XK_m, setlayout, {.v = &layouts[2]}},
@@ -95,29 +95,28 @@ static const Key keys[] = {
     // { MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
     {MODKEY, XK_0, view, {.ui = ~0}},
     {MODKEY | ShiftMask, XK_0, tag, {.ui = ~0}},
-    {MODKEY, XK_s, focusmon, {.i = -1}},
-    {MODKEY, XK_d, focusmon, {.i = +1}},
+    {MODKEY, XK_s, focusmon, {.i = -1}}, {MODKEY, XK_d, focusmon, {.i = +1}},
     {MODKEY | ShiftMask, XK_comma, tagmon, {.i = -1}},
     {MODKEY | ShiftMask, XK_period, tagmon, {.i = +1}},
     TAGKEYS(XK_1, 0) TAGKEYS(XK_2, 1) TAGKEYS(XK_3, 2) TAGKEYS(XK_4, 3)
         TAGKEYS(XK_5, 4) TAGKEYS(XK_6, 5) TAGKEYS(XK_7, 6) TAGKEYS(XK_8, 7)
-            TAGKEYS(XK_9, 8){MODKEY | ShiftMask, XK_q, quit, {0}},
-};
+            TAGKEYS(XK_9, 8)
 
-/* button definitions */
-/* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle,
- * ClkClientWin, or ClkRootWin */
-static const Button buttons[] = {
-    /* click                event mask      button          function argument */
-    {ClkLtSymbol, 0, Button1, setlayout, {0}},
-    {ClkLtSymbol, 0, Button3, setlayout, {.v = &layouts[2]}},
-    {ClkWinTitle, 0, Button2, zoom, {0}},
-    {ClkStatusText, 0, Button2, spawn, {.v = termcmd}},
-    {ClkClientWin, MODKEY, Button1, movemouse, {0}},
-    {ClkClientWin, MODKEY, Button2, togglefloating, {0}},
-    {ClkClientWin, MODKEY, Button3, resizemouse, {0}},
-    {ClkTagBar, 0, Button1, view, {0}},
-    {ClkTagBar, 0, Button3, toggleview, {0}},
-    {ClkTagBar, MODKEY, Button1, tag, {0}},
-    {ClkTagBar, MODKEY, Button3, toggletag, {0}},
-};
+    /* button definitions */
+    /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle,
+     * ClkClientWin, or ClkRootWin */
+    static const Button buttons[] = {
+        /* click                event mask      button          function
+           argument */
+        {ClkLtSymbol, 0, Button1, setlayout, {0}},
+        {ClkLtSymbol, 0, Button3, setlayout, {.v = &layouts[2]}},
+        {ClkWinTitle, 0, Button2, zoom, {0}},
+        {ClkStatusText, 0, Button2, spawn, {.v = termcmd}},
+        {ClkClientWin, MODKEY, Button1, movemouse, {0}},
+        {ClkClientWin, MODKEY, Button2, togglefloating, {0}},
+        {ClkClientWin, MODKEY, Button3, resizemouse, {0}},
+        {ClkTagBar, 0, Button1, view, {0}},
+        {ClkTagBar, 0, Button3, toggleview, {0}},
+        {ClkTagBar, MODKEY, Button1, tag, {0}},
+        {ClkTagBar, MODKEY, Button3, toggletag, {0}},
+    };
