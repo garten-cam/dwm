fc8badda4926568e641932ffd210a4bb24f99ba7
Camilo Garcia <camilo_7@msn.com>
2025-07-03 16:19:55 +0200

    fix scratchpad

--- dwm.c
+++ dwm.c
@@ -166,9 +166,9 @@ typedef struct {
   unsigned int tags;
   int isfloating;
   int monitor;
+	const char scratchkey;
   int floatx, floaty, floatw, floath;
   int floatborderpx;
-  const char scratchkey;
 } Rule;
 
 /* function declarations */
@@ -2013,17 +2013,18 @@ void updatewmhints(Client *c) {
 }
 
 void cycleview(const Arg *arg) {
-	 unsigned int newtag ;
-	if (arg->ui) { /* if ui is 1 goto the left if 0 goto the right */
-	    newtag = selmon->tagset[selmon->seltags] >> 1;
-	    if (newtag == 0) newtag = (1 << (LENGTH(tags) - 1));
-	}
-	else{
-	    newtag = selmon->tagset[selmon->seltags] << 1;
-	    if (newtag > ( 1 << (LENGTH(tags) - 1))) newtag = 1;
-	}
-    Arg a = { .ui = newtag};
-    view(&a);
+  unsigned int newtag;
+  if (arg->ui) { /* if ui is 1 goto the left if 0 goto the right */
+    newtag = selmon->tagset[selmon->seltags] >> 1;
+    if (newtag == 0)
+      newtag = (1 << (LENGTH(tags) - 1));
+  } else {
+    newtag = selmon->tagset[selmon->seltags] << 1;
+    if (newtag > (1 << (LENGTH(tags) - 1)))
+      newtag = 1;
+  }
+  Arg a = {.ui = newtag};
+  view(&a);
 }
 
 void view(const Arg *arg) {
