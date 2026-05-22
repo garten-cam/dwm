7fd63dd9e635450911208dcc4e5e5c96bd7249d7
Camilo Garcia <camilo_7@msn.com>
2026-05-22 16:17:38 +0200

    Z13 config

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx = 3; /* border pixel of windows */
+static const unsigned int borderpx = 2; /* border pixel of windows */
 static const unsigned int gappx = 4;    /*     between windows */
 static const unsigned int snap = 32;    /* snap pixel */
 static const int showbar = 1;           /* 0 means no bar */
@@ -12,13 +12,11 @@ static const char col_gray1[] = "#222222";
 static const char col_gray2[] = "#444444";
 static const char col_gray3[] = "#bbbbbb";
 static const char col_gray4[] = "#eeeeee";
-static const char col_cyan[] = "#8aadf4";
-static const char col_green[] = "#40a02b";
-static const char col_white[] = "#cdd6f4";
+static const char col_cyan[] = "#82aaFF";
 static const char *colors[][3] = {
     /*               fg         bg         border   */
-    [SchemeNorm] = {col_gray3, col_gray1, col_white},
-    [SchemeSel] = {col_gray4, col_cyan, col_green},
+    [SchemeNorm] = {col_gray3, col_gray1, col_gray2},
+    [SchemeSel] = {col_gray4, col_cyan, col_cyan},
 };
 
 /* tagging */
